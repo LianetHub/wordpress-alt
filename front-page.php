bf9afa1fd204e1bea1ac84d8d253806daceb52cc
lianet <jack.tolmak@yandex.ru>
2025-06-07 19:07:40 +0300

    add offer block

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@ get_header();
 
 <?php require_once(TEMPLATE_PATH . '_promo.php'); ?>
 <?php require_once(TEMPLATE_PATH . '_nums.php'); ?>
+<?php require_once(TEMPLATE_PATH . '_offer.php'); ?>
 
 
 <?php get_footer(); ?>
---
+++ templates/_offer.php
@@ -0,0 +1,16 @@
+<section class="offer">
+    <div class="offer__image">
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/offer.jpg" class="cover-image" alt="Фото ноутбука">
+    </div>
+    <div class="offer__content">
+        <div class="container">
+            <div class="offer__body">
+                <h2 class="offer__title title">Получить коммерческое предложение</h2>
+                <p class="offer__subtitle">Оставьте заявку и получите расчет стоимости в течение 48х часов бесплатно</p>
+                <div class="offer__form">
+                    <?= do_shortcode('[contact-form-7 id="77ee36a" title="Контактная форма"]'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
