ec6de2cd19fa66d5b0f8add4e619dcea0eb60676
lianet <jack.tolmak@yandex.ru>
2025-06-18 18:59:59 +0300

    add taxonomy fix

--- templates/_industries.php
+++ templates/_industries.php
@@ -1,24 +1,36 @@
-<?php if (have_rows('industry_slides')): ?>
+<?php
+$terms = get_terms(array(
+    'taxonomy'   => 'project_industry',
+    'hide_empty' => false,
+    'orderby'    => 'name',
+    'order'      => 'ASC',
+));
+
+if (!empty($terms) && !is_wp_error($terms)) :
+?>
     <div class="industries">
         <div class="container">
             <div class="industries__body">
                 <div class="industries__slider swiper">
                     <div class="swiper-wrapper">
-                        <?php while (have_rows('industry_slides')): the_row();
+                        <?php foreach ($terms as $term) :
+                            $term_name = esc_html($term->name);
+                            $term_link = esc_url(get_term_link($term));
 
-                            $image_url = get_sub_field('slide_image');
-                            $caption_text = get_sub_field('slide_caption');
-                            $link_array = get_sub_field('slide_link');
+                            $background_image_url = get_field('promo_background', 'project_industry_' . $term->term_id) ?? '';
 
-                            $link_url = isset($link_array['url']) ? $link_array['url'] : '';
-                            $link_title = isset($link_array['title']) ? $link_array['title'] : '';
-                            $link_target = isset($link_array['target']) ? $link_array['target'] : '_self'; // '_self' по умолчанию
+                            $caption_text = $term_name;
+                            $link_url = $term_link;
+                            $link_title = $term_name;
+                            $link_target = '_self';
                         ?>
                             <a href="<?php echo esc_url($link_url); ?>" class="industries__slide swiper-slide" target="<?php echo esc_attr($link_target); ?>" title="<?php echo esc_attr($link_title); ?>">
-                                <img src="<?php echo esc_url($image_url); ?>" class="cover-image" alt="<?php echo esc_attr($caption_text); ?>">
+                                <? if ($background_image_url) : ?>
+                                    <img src="<?php echo esc_url($background_image_url); ?>" class="cover-image" alt="<?php echo esc_attr($caption_text); ?>">
+                                <?php endif; ?>
                                 <span class="industries__slide-caption title-sm"><?php echo esc_html($caption_text); ?></span>
                             </a>
-                        <?php endwhile; ?>
+                        <?php endforeach; ?>
                     </div>
                 </div>
                 <button type="button" class="industries__slider-prev swiper-button-prev"></button>
